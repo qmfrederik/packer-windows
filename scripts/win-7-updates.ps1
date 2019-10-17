597e249cbe104e821dc2e70632776bcd7c09da82
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-10-17 11:07:49 +0200

    Fix typo

--- scripts/win-7-updates.ps1
+++ scripts/win-7-updates.ps1
@@ -4,7 +4,7 @@ Write-Host "Downloading Windows 7 Service Pack 1"
 (New-Object Net.WebClient).DownloadFile("https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe", "C:\Updates\windows6.1-KB976932-X64.exe")
 
 Write-Host "Installing Windows 7 Service Pack 1"
-Start-Proces -FilePath "C:\Updates\Windows6.1-KB976932-X64.exe" -ArgumentList "/unattend /nodialog /norestart" -Wait
+Start-Process -FilePath "C:\Updates\Windows6.1-KB976932-X64.exe" -ArgumentList "/unattend /nodialog /norestart" -Wait
 
 Write-Host "Downloading April 2015 Servicing Stack Update for Windows 7"
 (New-Object Net.WebClient).DownloadFile("https://download.microsoft.com/download/5/D/0/5D0821EB-A92D-4CA2-9020-EC41D56B074F/Windows6.1-KB3020369-x64.msu", "C:\Updates\Windows6.1-KB3020369-x64.msu")
