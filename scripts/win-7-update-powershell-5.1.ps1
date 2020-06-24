c65ecf8946cf43b642060b68843c2480f716f988
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-06-24 23:39:16 +0200

    Try to enable TLS 1.2

--- scripts/win-7-update-powershell-5.1.ps1
+++ scripts/win-7-update-powershell-5.1.ps1
@@ -38,6 +38,18 @@ Start-Process -FilePath "wusa.exe" -ArgumentList "C:\Updates\$kbid.msu /extract:
 Write-Host "$(Get-Date -Format G): Installing $update"
 Start-Process -FilePath "dism.exe" -ArgumentList "/online /add-package /PackagePath:C:\Updates\$kbid.cab /quiet /norestart /LogPath:C:\Windows\Temp\$kbid.log" -Wait
 
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2'
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client'
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server'
+New-ItemProperty -path 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name Enabled -value 1 -PropertyType 'DWord' -Force
+New-ItemProperty -path 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name DisabledByDefault -value 0 -PropertyType 'DWord' -Force
+New-ItemProperty -path 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name Enabled -value 1 -PropertyType 'DWord' -Force
+New-ItemProperty -path 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name DisabledByDefault -value 0 -PropertyType 'DWord' -Force
+
+New-ItemProperty -path 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name DefaultSecureProtocols -value 2048 -PropertyType 'DWord' -Force
+New-ItemProperty -path 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name DefaultSecureProtocols -value 2048 -PropertyType 'DWord' -Force
+New-ItemProperty -path 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings' -name SecureProtocols -value 2048 -PropertyType 'DWord' -Force
+
 Remove-Item -LiteralPath "C:\Updates" -Force -Recurse
 
 Write-Host "$(Get-Date -Format G): Finished installing Windows Management Framework 5.1. The VM will now reboot and continue the installation process."
